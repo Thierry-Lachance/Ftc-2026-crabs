338b355ada038e94b784b95d8c5c8639ae3cfa57
Thierry Lachance <lachancethierry6@gmail.com>
2025-09-08 12:46:07 -0400

    base teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopBase.java
@@ -0,0 +1,150 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+@TeleOp
+public class TeleopBase extends LinearOpMode {
+    GoBildaPinpointDriver odo; // Declare OpMode member for the Odometry Computer
+    DriveToPoint nav = new DriveToPoint(this); //OpMode member for the point-to-point navigation class
+
+
+    DcMotor frontLeftMotor;
+    DcMotor backLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backRightMotor;
+
+
+    enum StateMachine {
+        WAITING_FOR_TARGET,
+        DRIVE_TO_TARGET_A,
+        DRIVE_TO_TARGET_B
+    }
+
+    static final Pose2D TARGET_A = new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0);
+    static final Pose2D TARGET_B = new Pose2D(DistanceUnit.INCH, 7, 0, AngleUnit.DEGREES, 0);
+
+
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        frontLeftMotor = hardwareMap.dcMotor.get("fl");
+        backLeftMotor = hardwareMap.dcMotor.get("bl");
+        frontRightMotor = hardwareMap.dcMotor.get("fr");
+        backRightMotor = hardwareMap.dcMotor.get("br");
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
+
+        imu.initialize(parameters);
+
+        odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
+        odo.setOffsets(80.0, -55); //these are tuned for 3110-0002-0001 Product Insight #1
+        odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.REVERSED);
+        odo.resetPosAndIMU();
+        nav.setDriveType(DriveToPoint.DriveType.MECANUM);
+
+        StateMachine stateMachine;
+        stateMachine = StateMachine.WAITING_FOR_TARGET;
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.addData("X offset", odo.getXOffset());
+        telemetry.addData("Y offset", odo.getYOffset());
+        telemetry.addData("Device Version Number:", odo.getDeviceVersion());
+        telemetry.addData("Device Scalar", odo.getYawScalar());
+        telemetry.update();
+
+        waitForStart();
+        resetRuntime();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+            telemetry.update();
+            odo.update();
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x * 0.7;
+
+            double up = (double) (gamepad1.dpad_up ? 1 : 0) /2;
+            double down = (double) (gamepad1.dpad_down ? 1 : 0) /2;
+            double left = (double) (gamepad1.dpad_left ? 1 : 0) /2;
+            double right = (double) (gamepad1.dpad_right ? 1 : 0) /2;
+
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;
+
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            if (stateMachine == StateMachine.WAITING_FOR_TARGET) {
+                frontLeftMotor.setPower(frontLeftPower + up - down - left + right);
+                backLeftMotor.setPower(backLeftPower + up - down + left - right);
+                frontRightMotor.setPower(frontRightPower + up - down + left - right);
+                backRightMotor.setPower(backRightPower + up - down - left + right);
+            } else {
+                frontLeftMotor.setPower(nav.getMotorPower(DriveToPoint.DriveMotor.LEFT_FRONT));
+                frontRightMotor.setPower(nav.getMotorPower(DriveToPoint.DriveMotor.RIGHT_FRONT));
+                backLeftMotor.setPower(nav.getMotorPower(DriveToPoint.DriveMotor.LEFT_BACK));
+                backRightMotor.setPower(nav.getMotorPower(DriveToPoint.DriveMotor.RIGHT_BACK));
+            }
+        //***********************DRIVING LOGIC************************//
+
+            if (gamepad1.right_bumper || gamepad1.left_bumper) {
+                odo.setPosition(new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0));
+            }
+            if (gamepad1.b) {
+                    stateMachine = StateMachine.DRIVE_TO_TARGET_B;
+                    nav.driveTo(odo.getPosition(), TARGET_B, (Math.abs(Math.sqrt((Math.abs(odo.getPosX()+odo.getPosY()))/2)/100))+0.3, 0);
+
+            }
+            else if(gamepad2.a){
+                stateMachine = StateMachine.DRIVE_TO_TARGET_A;
+                nav.driveTo(odo.getPosition(), TARGET_A, (Math.abs(Math.sqrt((Math.abs(odo.getPosX()+odo.getPosY()))/2)/100))+0.3, 0);
+            }
+
+            else {
+                stateMachine = StateMachine.WAITING_FOR_TARGET;
+            }
+
+
+
+        }
+    }
+   }   // end class
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zero.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+@TeleOp
+public class Zero extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        waitForStart();
+        resetRuntime();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+
+        }
+    }
+}
